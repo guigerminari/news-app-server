17db9ce6c9d448e9ff80f5224ea82559b40c4a33
gags.guilherme@gmail.com <gags.guilherme@gmail.com>
2022-01-05 21:11:14 -0300

    Finalizado

--- src/index.js
+++ src/index.js
@@ -12,16 +12,16 @@ app.use((req, res, next) => {
 });
 
 app.get("/news",(request,response) =>{
-  console.log('Entrou');
     
-    const newsapi = new NewsAPI('94dcb758a5204f9fb10a3a10a64c66eb');
+  const newsapi = new NewsAPI('94dcb758a5204f9fb10a3a10a64c66eb');
 
-    newsapi.v2.topHeadlines({
-        country: 'br'
-      }).then(resp => {
-        return response.json(resp);
-        
-      });
+  newsapi.v2.topHeadlines({
+      country: 'br'
+    }).then(resp => {
+      console.log('All ' + resp.totalResults);
+      return response.json(resp);
+      
+    });
 });
 
 app.get("/news/:search",(request,response) =>{
@@ -30,10 +30,10 @@ app.get("/news/:search",(request,response) =>{
 
     newsapi.v2.everything({
         language: 'br',
-        qInTitle: search,
+        q: search,
         sortBy: "publishedAt"
       }).then(resp => {
-
+        console.log('Search: ' + search + ' ' + resp.totalResults);
         return response.json(resp);
         
       });
